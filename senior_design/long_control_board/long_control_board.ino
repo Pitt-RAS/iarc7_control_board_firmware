7293915ccfa94a23ff3a09fd8f13058d9a95757a
Ritesh Misra <riteshlogin@gmail.com>
2018-03-23 18:03:55 -0400

    Remove identifier
    Added timestamp offsets and removed identifier for nano_transcriber
    logic

--- senior_design/long_control_board/long_control_board.ino
+++ senior_design/long_control_board/long_control_board.ino
@@ -1,7 +1,7 @@
 #include <ros.h>
 #include <SoftwareSerial.h>
 #include "Bitcraze_PMW3901.h"
-#include <sensor_msgs/Range.h>
+//#include <sensor_msgs/Range.h>
 #include <iarc7_msgs/Nano.h>
 #include <Wire.h>
 #include <VL53L0X.h>
@@ -17,14 +17,14 @@ ros::Publisher sensors_pub("nano_data", &sensors_msgs);
 
 void setup()
 {
-   
+    // Don't forget to run roscore!
     nh.getHardware()->setBaud(115200);
     nh.initNode();
     nh.advertise(sensors_pub);
     //nh.advertise(rangefinder_pub);
     
     SoftSrial.begin(115200);
-    //Serial.begin(115200);
+    Serial.begin(115200);
     if (!flow.begin()) {
       //Serial.println("Initialization of the flow sensor failed");
       //while(1) { }
@@ -43,9 +43,7 @@ void loop()
     static unsigned long current_time = micros();
     
     if(SoftSrial.available() >= 9)
-    {
-
-      
+    { 
           while(0x59 != SoftSrial.read());
           if(0x59 == SoftSrial.read())
           //if((0x59 == SoftSrial.read()) && (0x59 == SoftSrial.read())) //Byte1 & Byte2
@@ -56,53 +54,54 @@ void loop()
               t2 += t1;
               if(t2 < 1000)
               {
-//                  Serial.print(t2);
-//                  Serial.print('\t');
-//                  Serial.print('\n');
+//                Serial.print(t2);
+//                Serial.print('\t');
+//                Serial.print('\n');
                   sensors_msgs.long_range = (float)(t2/100.0);
                   sensors_msgs.msg_received = nh.now();
-                  sensors_msgs.identifier =  2;
-                  sensors_pub.publish(&sensors_msgs);
               }
               t1 = SoftSrial.read(); //Byte5
               t2 = SoftSrial.read(); //Byte6
               t2 <<= 8;
               t2 += t1;
-              if(t2 < 1000)
-              {
-                  //Serial.print(t2);
-                  //Serial.print('\n');
-              }
+
               for(int i=0; i<3; i++)
               {
                   SoftSrial.read(); ////Byte7,8,9
               }
 
-            
-              if(micros() - current_time >= 25333)
+              
+              if(sensor.checkReady())
               {
-                  current_time = micros();
-                  sensors_msgs.msg_received = nh.now();
                   sensors_msgs.short_range = sensor.readRangeContinuousMillimeters()/1000.0;
-                  sensors_msgs.identifier = 1; 
-                  sensors_pub.publish(&sensors_msgs);  
-                        
+                  sensors_msgs.short_range_offset = micros() - current_time;
+                               
+              }
+              else
+              {
+                sensors_msgs.short_range_offset = 0;
               }
                 
               flow.readMotionCount(&deltaX, &deltaY);
       
               sensors_msgs.deltaX = deltaX;
               sensors_msgs.deltaY = deltaY;
-              sensors_msgs.identifier = 4;
-              sensors_msgs.msg_received = nh.now();
-              
+              sensors_msgs.flow_board_offset = micros() - current_time;
+              //Serial.print("\n");
+              //Serial.print("Two\n");
+              //Serial.print(long_offset);
               sensors_pub.publish(&sensors_msgs);
               //Serial.print("X: ");
               //Serial.print(deltaX);
               //Serial.print(", Y: ");
               //Serial.print(deltaY);
               //Serial.print("\n");
-    
+              current_time = micros();
+//
+              //Serial.print(loop_check3);
+              //Serial.print("\n");
+
+              
               nh.spinOnce();
           }
        }
