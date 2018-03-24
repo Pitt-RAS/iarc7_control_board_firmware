ec756856e84c72eb4bb37053fcd0d7b97c90297d
Ritesh Misra <riteshlogin@gmail.com>
2018-03-24 18:41:09 -0400

    Added battery publisher
    Now looks at battery values

--- senior_design/long_control_board/long_control_board.ino
+++ senior_design/long_control_board/long_control_board.ino
@@ -33,9 +33,11 @@ void setup()
     sensor.init();
     sensor.setTimeout(500);
     sensor.startContinuous();
+
 }
 
 int16_t deltaX,deltaY;
+int voltagePin = A0;
 
 void loop()
 {
@@ -69,8 +71,7 @@ void loop()
               {
                   SoftSrial.read(); ////Byte7,8,9
               }
-
-              
+             
               if(sensor.checkReady())
               {
                   sensors_msgs.short_range = sensor.readRangeContinuousMillimeters()/1000.0;
@@ -87,6 +88,22 @@ void loop()
               sensors_msgs.deltaX = deltaX;
               sensors_msgs.deltaY = deltaY;
               sensors_msgs.flow_board_offset = micros() - current_time;
+
+              int voltage_sensor_average = 20;
+              uint32_t ADCCountsVolts = 0;
+
+              
+              for(int i = 0; i < voltage_sensor_average; i++) {
+                ADCCountsVolts += analogRead(voltagePin);
+                delayMicroseconds(50);
+              }
+              
+              sensors_msgs.battery_offset = micros() - current_time;
+              
+              ADCCountsVolts = ADCCountsVolts*5*4.9*12.4/voltage_sensor_average;
+
+              sensors_msgs.battery_voltage = (float)ADCCountsVolts/(1024*14.16);
+              
               //Serial.print("\n");
               //Serial.print("Two\n");
               //Serial.print(long_offset);
@@ -100,8 +117,7 @@ void loop()
 //
               //Serial.print(loop_check3);
               //Serial.print("\n");
-
-              
+            
               nh.spinOnce();
           }
        }
